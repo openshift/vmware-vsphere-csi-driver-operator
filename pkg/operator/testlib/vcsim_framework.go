225238331d0d4b9ad293e69d3a3a900d432ed66d
vr4manta <vr4manta@gmail.com>
2025-07-17 14:34:31 -0400

    Added ability to ignore non vSphere nodes in hybrid env

--- pkg/operator/testlib/vcsim_framework.go
+++ pkg/operator/testlib/vcsim_framework.go
@@ -180,7 +180,8 @@ func DefaultNodes() []*v1.Node {
 	for _, vm := range DefaultVMs {
 		node := Node(vm.name, WithProviderID("vsphere://"+vm.uuid))
 		node.Labels = map[string]string{
-			"kubernetes.io/os": "linux",
+			"kubernetes.io/os":                "linux",
+			"node.openshift.io/platform-type": "vsphere",
 		}
 		nodes = append(nodes, node)
 	}
