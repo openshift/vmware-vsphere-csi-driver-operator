5ec69e63d664cfc089c3e0825500bc0536286d85
vr4manta <vr4manta@gmail.com>
2026-04-14 12:31:44 -0400

    Added OTE tests

--- Makefile
+++ Makefile
@@ -1,5 +1,9 @@
-all: build
+all: build build-tests-ext
 .PHONY: all
+
+# Override GO_PACKAGE detection to use only the root module path
+# (workspace mode returns multiple modules which breaks build-machinery-go)
+GO_PACKAGE := github.com/openshift/vmware-vsphere-csi-driver-operator
 
 # Include the library makefile
 include $(addprefix ./vendor/github.com/openshift/build-machinery-go/make/, \
@@ -26,8 +30,21 @@ IMAGE_REGISTRY?=registry.ci.openshift.org
 # It will generate target "image-$(1)" for building the image and binding it as a prerequisite to target "images".
 $(call build-image,vmware-vsphere-csi-driver-operator,$(IMAGE_REGISTRY)/ocp/4.8:vmware-vsphere-csi-driver-operator,./Dockerfile.openshift,.)
 
+# Build the test extension binary from the openshift-tests workspace
+build-tests-ext:
+	cd openshift-tests && $(GO) build $(GO_BUILD_FLAGS) -o ../vmware-vsphere-csi-driver-operator-tests-ext ./cmd/vmware-vsphere-csi-driver-operator-tests-ext
+.PHONY: build-tests-ext
+
+# Vendor dependencies using workspace-level vendoring
+# This creates a single shared vendor directory for both modules
+vendor:
+	$(GO) mod tidy
+	cd openshift-tests && $(GO) mod tidy
+	$(GO) work vendor
+.PHONY: vendor
+
 clean:
-	$(RM) vmware-vsphere-csi-driver-operator
+	$(RM) vmware-vsphere-csi-driver-operator vmware-vsphere-csi-driver-operator-tests-ext
 .PHONY: clean
 
 GO_TEST_PACKAGES :=./pkg/... ./cmd/...
---
+++ openshift-tests/cmd/vmware-vsphere-csi-driver-operator-tests-ext/main.go
@@ -0,0 +1,85 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"regexp"
+	"strings"
+
+	"github.com/openshift-eng/openshift-tests-extension/pkg/cmd"
+	"github.com/openshift-eng/openshift-tests-extension/pkg/extension"
+	"github.com/openshift-eng/openshift-tests-extension/pkg/extension/extensiontests"
+	"github.com/openshift-eng/openshift-tests-extension/pkg/ginkgo"
+	"github.com/spf13/cobra"
+	"github.com/spf13/pflag"
+	utilflag "k8s.io/component-base/cli/flag"
+	"k8s.io/component-base/logs"
+
+	_ "github.com/openshift/vmware-vsphere-csi-driver-operator/openshift-tests/e2e"
+)
+
+func main() {
+	logs.InitLogs()
+	defer logs.FlushLogs()
+	pflag.CommandLine.SetNormalizeFunc(utilflag.WordSepNormalizeFunc)
+
+	// Create our registry of openshift-tests extensions
+	extensionRegistry := extension.NewRegistry()
+	kubeTestsExtension := extension.NewExtension("openshift", "payload", "vmware-vsphere-csi-driver-operator")
+	extensionRegistry.Register(kubeTestsExtension)
+
+	// Carve up the kube tests into our openshift suites...
+	kubeTestsExtension.AddSuite(extension.Suite{
+		Name: "vvcdo/conformance/parallel",
+		Parents: []string{
+			"openshift/conformance/parallel",
+		},
+		Qualifiers: []string{`!labels.exists(l, l == "Serial") && labels.exists(l, l == "Conformance")`},
+	})
+
+	kubeTestsExtension.AddSuite(extension.Suite{
+		Name: "vvcdo/conformance/serial",
+		Parents: []string{
+			"openshift/conformance/serial",
+		},
+		Qualifiers: []string{`labels.exists(l, l == "Serial") && labels.exists(l, l == "Conformance")`},
+	})
+
+	// Build our specs from ginkgo
+	specs, err := ginkgo.BuildExtensionTestSpecsFromOpenShiftGinkgoSuite()
+	if err != nil {
+		panic(err)
+	}
+
+	// Let's scan for tests with a platform label and create the rule for them such as [platform:vsphere]
+	foundPlatforms := make(map[string]string)
+	for _, test := range specs.Select(extensiontests.NameContains("[platform:")).Names() {
+		re := regexp.MustCompile(`\[platform:[a-z]*]`)
+		match := re.FindStringSubmatch(test)
+		for _, platformDef := range match {
+			if _, ok := foundPlatforms[platformDef]; !ok {
+				platform := platformDef[strings.Index(platformDef, ":")+1 : len(platformDef)-1]
+				foundPlatforms[platformDef] = platform
+				specs.Select(extensiontests.NameContains(platformDef)).
+					Include(extensiontests.PlatformEquals(platform))
+			}
+		}
+
+	}
+
+	kubeTestsExtension.AddSpecs(specs)
+
+	// Cobra stuff
+	root := &cobra.Command{
+		Long: "vmware vSphere CSI Driver Operator tests extension for OpenShift",
+	}
+
+	root.AddCommand(cmd.DefaultExtensionCommands(extensionRegistry)...)
+
+	if err := func() error {
+		return root.Execute()
+	}(); err != nil {
+		fmt.Fprintf(os.Stderr, "Error: %v\n", err)
+		os.Exit(1)
+	}
+}
